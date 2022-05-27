068a2dea26fa0796289e55dde60fd3c2b92fb395
Viacheslav Miroshko <viacheslav.miroshko@auto1.com>
2022-05-27 17:22:39 +0300

    fix(curl): fixed write-out

--- post-command.sh
+++ post-command.sh
@@ -2,21 +2,21 @@
 
 set -e
 token=$1
-branch=$2
+base_branch_name=$2
 path=$3
 repo=$GITHUB_REPOSITORY
 username=$GITHUB_ACTOR
-branch_name="simple-pr-changes"
+new_branch_name="simple-pr-changes"
 
 if [ -z "$token" ]; then
     echo "Token is not defined."
     exit 1
 fi
 
-if [ -z "$branch" ]; then
+if [ -z "$base_branch_name" ]; then
     echo "Can't find main branch name. Setting it to default."
-    branch="main"
-    echo "Main branch changed to $branch."
+    base_branch_name="main"
+    echo "Main branch changed to $base_branch_name."
 fi
 
 path_value=${path%?}
@@ -34,15 +34,10 @@ then
     git commit -a -m "new(app): automatic changes" --signoff
     git push authenticated -f
     echo "https://api.github.com/repos/$repo/pulls"
-    response=$(curl --write-out "%{message}\n" -X POST -H "Content-Type: application/json" -H "Authorization: token $token" \
-         --data '{"title":"new(app): automatic changes","head": "'"$branch_name"'","base":"'"$branch"'", "body":""}' \
+    response=$(curl --write-out "%{response_code}\n" -X POST -H "Content-Type: application/json" -H "Authorization: token $token" \
+         --data '{"title":"new(app): automatic changes","head": "'"$new_branch_name"'","base":"'"$base_branch_name"'", "body":"Automatic PR."}' \
          "https://api.github.com/repos/$repo/pulls")
-    echo "$response."
-
-    if [[ "$response" == *"already exist"* ]]; then
-        echo "Pull request has been already opened. Changes were pushed to the existing PR instead."
-        exit 0
-    fi
+    echo "Response code: $response."
 else
     echo "No changes were detected."
     exit 0
