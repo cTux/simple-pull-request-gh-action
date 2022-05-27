24a49f395a2cbc2463c0635e6547897f11ae6042
Viacheslav Miroshko <viacheslav.miroshko@auto1.com>
2022-05-27 17:42:35 +0300

    feat(action): custom commit message

--- post-command.sh
+++ post-command.sh
@@ -2,8 +2,9 @@
 
 set -e
 token=$1
-base_branch_name=$2
-path=$3
+commit_message=$2
+base_branch_name=$3
+path=$4
 repo=$GITHUB_REPOSITORY
 username=$GITHUB_ACTOR
 new_branch_name="simple-pr-changes"
@@ -19,6 +20,12 @@ if [ -z "$base_branch_name" ]; then
     echo "Main branch changed to $base_branch_name."
 fi
 
+if [ -z "$commit_message" ]; then
+    echo "Can't find commit message. Setting it to default."
+    commit_message="chore(app): changes"
+    echo "Commit message is set to $commit_message."
+fi
+
 path_value=${path%?}
 if [ -n "$path_value" ]; then
     echo "Change directory to $path_value."
@@ -31,11 +38,11 @@ then
     echo "Changes detected."
     git remote add authenticated "https://$username:$token@github.com/$repo.git"
     git add -A
-    git commit -a -m "new(app): automatic changes" --signoff
+    git commit -a -m "$commit_message" --signoff
     git push authenticated -f
     echo "https://api.github.com/repos/$repo/pulls"
     response=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token $token" \
-         --data '{"title":"new(app): automatic changes","head": "'$new_branch_name'","base":"'$base_branch_name'", "body":"Automatic PR."}' \
+         --data '{"title":"'$commit_message'","head": "'$new_branch_name'","base":"'$base_branch_name'", "body":"Automatic Pull Request."}' \
          "https://api.github.com/repos/$repo/pulls")
     echo "Response: $response"
 else
