d6c022534a491fcca8b53165970475cacb812a47
Viacheslav Miroshko <viacheslav.miroshko@auto1.com>
2022-05-27 21:40:34 +0300

    feat(action): changes-branch-name (#8)

--- post-command.sh
+++ post-command.sh
@@ -4,10 +4,10 @@ set -e
 token=$1
 commit_message=$2
 base_branch_name=$3
-path=$4
+base_pr_name=$4
+path=$5
 repo=$GITHUB_REPOSITORY
 username=$GITHUB_ACTOR
-new_branch_name="simple-pr-changes"
 
 if [ -z "$token" ]; then
     echo "Token is not defined."
@@ -18,6 +18,12 @@ if [ -z "$base_branch_name" ]; then
     echo "Can't find main branch name. Setting it to default."
     base_branch_name="main"
     echo "Main branch changed to $base_branch_name."
+fi
+
+if [ -z "$base_pr_name" ]; then
+    echo "Can't find changes branch name. Setting it to default."
+    base_pr_name="simple-pr-changes"
+    echo "Changes branch changed to $base_pr_name."
 fi
 
 if [ -z "$commit_message" ]; then
@@ -43,11 +49,11 @@ then
 
     echo "https://api.github.com/repos/$repo/pulls"
     echo "Commit message: $commit_message"
-    echo "Head branch: $new_branch_name"
+    echo "Head branch: $base_pr_name"
     echo "Base branch: $base_branch_name"
 
     response=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token $token" \
-         --data '{"title":"'"$commit_message"'","head": "'$new_branch_name'","base":"'$base_branch_name'", "body":"Automatic Pull Request."}' \
+         --data '{"title":"'"$commit_message"'","head": "'$base_pr_name'","base":"'$base_branch_name'", "body":"Automatic Pull Request."}' \
          "https://api.github.com/repos/$repo/pulls")
     echo "Response: $response"
 else
