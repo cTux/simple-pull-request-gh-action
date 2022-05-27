35ba1c1b7b5ada05830d97d14d82b7fb4255d64b
Viacheslav Miroshko <viacheslav.miroshko@auto1.com>
2022-05-27 22:53:14 +0300

    fix(command): not defined

--- command.sh
+++ command.sh
@@ -4,6 +4,11 @@ set -e
 command=$1
 path=$2
 
+if [ -z "$command" ]; then
+    echo "Command is not defined."
+    exit 1
+fi
+
 path_value=${path%?}
 if [ -n "$path_value" ]; then
     echo "Change directory to $path_value."
